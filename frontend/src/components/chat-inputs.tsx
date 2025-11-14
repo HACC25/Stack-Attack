59109b02d7b1f0af961cc431f6475d8697011781
PsuedoBlessings <zmasike@gmail.com>
2025-11-13 16:11:04 -1000

    Added Quick Action Chat Button

--- frontend/src/components/chat-inputs.tsx
+++ frontend/src/components/chat-inputs.tsx
@@ -6,12 +6,21 @@ import {
 import { FileButton } from "./file-button";
 import { DictationButton } from "./dictation-button";
 import TextInput from './text-input';
+import { QuickChatButton, type QuickChatButtonProp } from './quick-chat-input';
 
-//Custom File Button Componenet
+const QuickchatButton_Example: QuickChatButtonProp[] = [{
+    cardTitle:"Blue Collar Worker"
+},
+{
+    cardTitle: "White Collar Worker"
+}]
 
 export function AppChatInput(){
     return(
-    <div className='m-5'>
+    <div className='m-5 flex flex-col gap-2'>
+        <div className='flex justify-center gap-3'>
+        {QuickchatButton_Example.map((quickActionButton) => (<QuickChatButton cardTitle={quickActionButton.cardTitle} cardDescription={quickActionButton.cardDescription} />))}
+        </div>
         <Card className='p-2'>
             <form>
                 <Field>
