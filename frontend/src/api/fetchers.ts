e6c59a9fb96b2070164097a475b0169080bd464f
Liam Tapper <97718694+tliam1@users.noreply.github.com>
2025-11-16 12:53:21 -1000

    correct env errors and add login redirect url

--- frontend/src/api/fetchers.ts
+++ frontend/src/api/fetchers.ts
@@ -1,6 +1,12 @@
 import { apiRequestCallback } from "./api"
 
-const BACKEND_URL = import.meta.env.BACKEND_URL
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+
+const LOGIN_REDIRECT = import.meta.env.VITE_LOGIN_REDIRECT_URL
+
+export const login_redirect = () => {
+    window.location = LOGIN_REDIRECT
+}
 
 export const fetch_chats = async () => {
     const response = await apiRequestCallback("/chats/", {
@@ -8,6 +14,12 @@ export const fetch_chats = async () => {
         baseUrl: BACKEND_URL,
         token: undefined
     })
+
+    // 403 == user not in db, 401 == token invalid / unauthorized
+    if (!response.ok && (response.status === 403 || response.status === 401)){ 
+        // Redirect to login url
+        login_redirect()
+    }
 
     return response
 }
