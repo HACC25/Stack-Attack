ef63ab31993e96d6ce981effc2f79dac29eebe94
Liam Tapper <97718694+tliam1@users.noreply.github.com>
2025-11-16 11:47:14 -1000

    actually return the response

--- frontend/src/api/fetchers.ts
+++ frontend/src/api/fetchers.ts
@@ -3,9 +3,11 @@ import { apiRequestCallback } from "./api"
 const BACKEND_URL = import.meta.env.BACKEND_URL
 
 export const fetch_chats = async () => {
-    await apiRequestCallback("/chats/", {
+    const response = await apiRequestCallback("/chats/", {
         method: "GET",
         baseUrl: BACKEND_URL,
         token: undefined
     })
+
+    return response
 }
